6c3fb4b5c268ee0447a89b26a429b1c73358f6a7
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-04-25 11:20:13 -0400

    nav to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 require('../../styles/Header.css');
 
 const Header = () => {
   return (
     <header>
       <h1> Emotions for Reddit </h1> 
+      <nav>
+        <h5>Front Page Analysis</h5>
+        <li><Link to="/chart/top">Top Posts</Link></li>
+        <li><Link to="/chart/hot">Hot Posts</Link></li>
+      </nav>
     </header>
   );
 }
