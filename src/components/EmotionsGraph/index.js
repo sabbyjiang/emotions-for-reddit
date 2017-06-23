b131d3492ad550403e10ec350fb49d3f5829d3eb
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-06-23 14:03:42 -0400

    some styling and mostly working tooltip

--- src/components/EmotionsGraph/index.js
+++ src/components/EmotionsGraph/index.js
@@ -6,13 +6,14 @@ import StackedBar from './StackedBar';
 require('../../../styles/EmotionsGraph.css');
 import Sorting from './Sorting';
 import { baseURL } from '../../../config';
+import ReactTooltip from 'react-tooltip'
 
 class EmotionsGraph extends Component {
   constructor(){
     super();
     this.state = {
       rawData: [],
-      currentPost: "Hover over a bar to see the title",
+      currentPost: {},
       currentData: [],
       currentSort: "total"
     }
@@ -35,7 +36,7 @@ class EmotionsGraph extends Component {
       .then(r => {
         this.setState({rawData: r.data, currentData: r.data[0]});
       })
-      .catch(err => alert("err", err));
+      .catch(err => console.log("err", err));
   }
 
   componentDidMount(){
@@ -63,6 +64,14 @@ class EmotionsGraph extends Component {
     this.setState({currentSort: newSort});
   }
 
+  getTonesForTooltip(tone){
+    return (
+      <li key={tone.tone_id}>
+        <span className="emph">{`${tone.tone_name}`}</span>{`: ${(tone.score*100).toFixed(2)}%`}
+      </li>
+    )
+  }
+
   render(){
     const margin = {top: 20, right: 40, bottom: 30, left: 40},
           width = 900 - margin.left - margin.right,
@@ -78,33 +87,44 @@ class EmotionsGraph extends Component {
     } else {
       return (
         <div className="App landing graph-page">
-          <div className="graph-titles">
-            <h2> Tone Analysis of {this.props.match.params.subredditName ? `r/${this.props.match.params.subredditName}'s` : "Reddit's"} Hot Page</h2>
+          <div className="graph-details-container">
+            <div className="graph-titles">
+              <h2> Tone Analysis of {this.props.match.params.subredditName ? `r/${this.props.match.params.subredditName}'s` : "Reddit's"}</h2>
 
-            <p className="post-title"> Title: {this.state.currentPost} </p>
-          </div>
-
-          <div className="graph-options-container">
-            <div className="graph-options">
-              <h5> Analysis Type: </h5>
-              <select onChange={this.changeCurrentData.bind(this)}>
-                <option value="0">Emotion Tone</option>
-                <option value="1">Language Tone</option>
-                <option value="2">Social Tone</option>
-              </select>
+              <p> Hover over the graph for more info </p>
+              <p> Click on a bar to open the specific thread </p>
             </div>
 
-            <div className="graph-options">
-              <h5> Sort By: </h5>
-              <Sorting currentData={this.state.currentData} changeSort={this.changeSort.bind(this)} currentSort={this.state.currentSort}/>
+            <div className="graph-options-container">
+              <div className="graph-options">
+                <h5> Analysis Type: </h5>
+                <select onChange={this.changeCurrentData.bind(this)}>
+                  <option value="0">Emotion Tone</option>
+                  <option value="1">Language Tone</option>
+                  <option value="2">Social Tone</option>
+                </select>
+              </div>
+
+              <div className="graph-options">
+                <h5> Sort By: </h5>
+                <Sorting currentData={this.state.currentData} changeSort={this.changeSort.bind(this)} currentSort={this.state.currentSort}/>
+              </div>
             </div>
           </div>
+          
           <div className="graph">
             <svg width={width + 50} height={height + 150}>
               <StackedBar {...params} data={this.state.currentData} setCurrPost={this.setCurrPost.bind(this)} currentSort={this.state.currentSort}/>
             </svg>
           </div>
           
+          <ReactTooltip type="info" id={"bar"}>
+            <span className="sr-title">{this.state.currentPost.title}</span>
+            <br /><br />
+            <ul className="tone-stats">
+              {this.state.currentPost.tones ? this.state.currentPost.tones.map((tone) => this.getTonesForTooltip(tone)) : ""}
+            </ul>
+          </ReactTooltip>
         </div>
       );
     }
